3440a3cb658184cb86b4eefb737bbe1eced40dbe
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2013-02-08 12:20:20 +0900

    create rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kakesan::Application.config.secret_token = '5b28f3d9dca74bc4324d5db61f30694c3d4e639dbb2600f9eb00921dd6427bf9df1fa58b45e25f9c56fe542cd3e2fe6026233edab0e821b8b0790b2de35afc97'
