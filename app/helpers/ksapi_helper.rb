5c75215ebab138ee52508eda5152c263f2e86eaa
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2013-02-19 14:56:32 +0900

    modify ruby version

--- app/helpers/ksapi_helper.rb
+++ app/helpers/ksapi_helper.rb
@@ -4,14 +4,14 @@ module KsapiHelper
   end
 
   def button_ksapi(action, title, position)
-    "ksapi://button(#{{buttonAction: action, buttonTitle: URI.escape(title), buttonPosition: position}.to_json})".strip
+    "ksapi://button(#{{buttonAction: action, buttonTitle: URI.escape(title), buttonPosition: position}.to_json})"
   end
 
   def login_ksapi
-    "ksapi://login(#{{}.to_json})".strip
+    "ksapi://login(#{{}.to_json})"
   end
 
   def logout_ksapi
-    "ksapi://logout(#{{}.to_json})".strip 
+    "ksapi://logout(#{{}.to_json})" 
   end
 end
