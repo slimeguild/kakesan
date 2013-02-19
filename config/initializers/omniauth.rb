f5b109cd6e1f49727a22f4303073c0be471eefe2
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2013-02-19 14:05:10 +0900

    fix bug

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.development?
     provider :twitter, "Pluj6uKMZ0fXvsOE3MmPQ", "082SmDfoWrcFy2FzzSIBKYIgoZbtDFuBSeg2GsAED0"
   elsif Rails.env.production?
-    provider :twiiter, "0bCVPZXNW2zNxII9GPaHiw", "Q5rRhOiMy5weCYeKX9cqEOVR2pgZsIm6qIPM79M74"
+    provider :twitter, "0bCVPZXNW2zNxII9GPaHiw", "Q5rRhOiMy5weCYeKX9cqEOVR2pgZsIm6qIPM79M74"
   end
 end
