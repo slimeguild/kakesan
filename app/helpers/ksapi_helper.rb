8a62d077608205260ef7f3af421d7ebafcf05594
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2013-02-19 14:29:03 +0900

    modify helper url

--- app/helpers/ksapi_helper.rb
+++ app/helpers/ksapi_helper.rb
@@ -1,17 +1,17 @@
 module KsapiHelper
   def load_ksapi(path, title)
-    "ksapi://load(#{{path: path, title: URI.escape(title)}.to_json})"
+    "ksapi://load(#{{path: path, title: URI.escape(title)}.to_json})".strip
   end
 
   def button_ksapi(action, title, position)
-    "ksapi://button(#{{buttonAction: action, buttonTitle: URI.escape(title), buttonPosition: position}.to_json})"
+    "ksapi://button(#{{buttonAction: action, buttonTitle: URI.escape(title), buttonPosition: position}.to_json})".strip
   end
 
   def login_ksapi
-    "ksapi://login(#{{}.to_json})"  
+    "ksapi://login(#{{}.to_json})".strip
   end
 
   def logout_ksapi
-    "ksapi://logout(#{{}.to_json})"  
+    "ksapi://logout(#{{}.to_json})".strip 
   end
 end
