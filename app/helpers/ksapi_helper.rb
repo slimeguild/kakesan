52dd543b1659c9e06f11422f8c490d0647e6848c
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2013-02-19 15:44:30 +0900

    url encode

--- app/helpers/ksapi_helper.rb
+++ app/helpers/ksapi_helper.rb
@@ -1,17 +1,17 @@
 module KsapiHelper
   def load_ksapi(path, title)
-    "ksapi://load(#{{path: path, title: URI.escape(title)}.to_json})".strip
+    URI.encode("ksapi://load(#{{path: path, title: URI.escape(title)}.to_json})".strip)
   end
 
   def button_ksapi(action, title, position)
-    "ksapi://button(#{{buttonAction: action, buttonTitle: URI.escape(title), buttonPosition: position}.to_json})"
+    URI.encode("ksapi://button(#{{buttonAction: action, buttonTitle: URI.escape(title), buttonPosition: position}.to_json})")
   end
 
   def login_ksapi
-    "ksapi://login(#{{}.to_json})"
+    URI.encode("ksapi://login(#{{}.to_json})")
   end
 
   def logout_ksapi
-    "ksapi://logout(#{{}.to_json})" 
+    URI.encode("ksapi://logout(#{{}.to_json})" )
   end
 end
