f73c130917bc06f18e5c17cc62e1e05b359c21d3
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2013-04-02 15:08:34 +0900

    不要なファイル削除

--- db/migrate/20130213125949_create_entries.rb
+++
@@ -1,9 +0,0 @@
-class CreateEntries < ActiveRecord::Migration
-  def change
-    create_table :entries do |t|
-      t.references :user
-      t.references :theme
-      t.timestamps
-    end
-  end
-end
