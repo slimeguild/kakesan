2a293b3496147e904334a4fd940a53668d722a63
Shinpei Kawahito <kawahito.shinpei@gmail.com>
2013-05-02 18:24:05 +0900

    fix bugs

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -5,7 +5,7 @@ class ChatsController < ApplicationController
     @chat = Chat.new(params[:chat])
     if @chat.save
       receiver = @chat.user == @chat.talk.user ? @chat.talk.theme.user : @chat.talk.user
-      API::PushNotification.new(reciever).chat_received(@chat) if receiver.notificatable?
+      API::PushNotification.new(receiver).chat_received(@chat) if receiver.notificatable?
       redirect_to talk_path(@chat.talk)
     else
       @talk = @chat.talk
